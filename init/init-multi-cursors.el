cdb0dba1477fc4541ad4dc84def9343205995c75
yzprofile <yzprofiles@gmail.com>
2016-06-29 11:17:22 +0800

    fix: https://github.com/magnars/multiple-cursors.el/pull/253

--- init/init-multi-cursors.el
+++ init/init-multi-cursors.el
@@ -1,5 +1,8 @@
 (require-package 'multiple-cursors)
 
+(defun mc/cursor-is-bar ()
+  nil)
+
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
